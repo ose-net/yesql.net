3a824ac3291ea44a29e94e9608155688c8ea4ebe
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-09 14:00:18 -0500

    Created TagNotFoundException class

---
+++ src/Exceptions/TagNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace YeSql.Net
+{
+    public class TagNotFoundException : Exception
+    {
+        public TagNotFoundException() : base(ExceptionMessages.TagNotFoundMessage) { }
+
+        public TagNotFoundException(string message) : base(message) { }
+    }
+}
