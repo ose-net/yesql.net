4d3072601bc4e1f3f21dccaf145f06391c8497b6
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-09 14:42:27 -0500

    Exceptions module created (#14)
    * Created 'YeSqlParserException'.
    * Created 'TagNotFoundException'.

---
+++ src/Exceptions/TagNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace YeSql.Net;
+
+public class TagNotFoundException : Exception
+{
+    public TagNotFoundException() : base(ExceptionMessages.TagNotFoundDefaultMessage) { }
+
+    public TagNotFoundException(string message) : base(message) { }
+}
---
+++ src/Exceptions/YeSqlParserException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace YeSql.Net;
+
+public class YeSqlParserException : Exception
+{
+    public YeSqlParserException() : base(ExceptionMessages.YeSqlParserDefaultMessage) { }
+
+    public YeSqlParserException(string message) : base(message) { }
+}
