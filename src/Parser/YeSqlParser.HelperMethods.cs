e6c959014a1778672ba512bcdb0770568b4d5da4
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-09 19:03:45 -0500

    Created YeSqlParser and YeSqlParser.HelperMethods without implementation

---
+++ src/Parser/YeSqlParser.HelperMethods.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YeSql.Net;
+
+/// <inheritdoc cref="{YeSqlParser}"/>
+public partial class YeSqlParser
+{
+	/// <summary>
+	/// Determines if a line of text is a comment line that does not contain a tag.
+	/// </summary>
+	/// <param name="line">The line of text to check.</param>
+	/// <returns>True if the line is a comment without a tag, false otherwise.</returns>
+	private bool IsCommentWithoutTag(string line)
+	{
+
+	}
+
+	/// <summary>
+	/// Determines if a line of text is a comment line that contains a tag.
+	/// </summary>
+	/// <param name="line">The line of text to check.</param>
+	/// <returns>True if the line is a comment with a tag, false otherwise.</returns>
+	private bool IsCommentWithTag(string line)
+	{
+
+	}
+
+	/// <summary>
+	/// Extracts the tag name from a line of text that is a comment with a tag.
+	/// </summary>
+	/// <param name="line">The line of text to extract the tag name from.</param>
+	/// <returns>The name of the tag, or an empty string if no tag name could be extracted.</returns>
+	private string ExtractTagName(string line)
+	{
+
+	}
+}
---
+++ src/Parser/YeSqlParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace YeSql.Net;
+
+/// <summary>
+/// A partial class that contains methods and properties related to parsing and validating SQL statements in a YeSQL file.
+/// </summary>
+public partial class YeSqlParser
+{
+    /// <summary>
+    /// A dictionary containing the SQL statements that have been parsed from the source code.
+    /// </summary>
+    private YeSqlDictionary _sqlStatements;
+
+    /// <summary>
+    /// The result of the validation performed on the SQL statements.
+    /// </summary>
+    internal YeSqlValidationResult validationResult;
+
+    /// <summary>
+    /// The name of the file from which the SQL statements were parsed.
+    /// </summary>
+    internal string sqlFileName;
+
+    /// <summary>
+    /// Parses an SQL file and returns a collection of SQL statements.
+    /// </summary>
+    /// <param name="source">The source code of the SQL file to parse.</param>
+    /// <param name="result">The validation result of the parsing process.</param>
+    /// <returns>A collection of SQL statements.</returns>
+    public IYeSqlCollection Parse(string source, YeSqlValidationResult result)
+    {
+
+    }
+
+}
