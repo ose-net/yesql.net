630b02fdb66e94de22f6c7c79d77572979e0e540
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2023-01-09 20:58:14 -0500

    Added `Collections` module (#21)
    * Added 'IYeSqlCollection' interface
    
    * Added 'YeSqlDictionary' internal class
    
    * Added 'TagModel' struct

---
+++ src/Collections/YeSqlDictionary.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YeSql.Net;
+
+/// <inheritdoc cref="IYeSqlCollection" />
+internal class YeSqlDictionary : IYeSqlCollection
+{
+    /// <summary>
+    /// A dictionary containing the SQL statements that have been parsed from the data source (e.g., a SQL file).
+    /// </summary>
+    private readonly Dictionary<string, string> _sqlStatements = new();
+
+    /// <inheritdoc />
+    public string this[string tagName]
+    {
+        get
+        {
+            if(tagName is null)
+                throw new ArgumentNullException(nameof(tagName));
+
+            if(_sqlStatements.TryGetValue(tagName, out var sqlStatement))
+                return sqlStatement;
+
+            throw new TagNotFoundException(string.Format(ExceptionMessages.TagNotFoundMessage, tagName));
+        }
+    }
+
+    /// <inheritdoc />
+    public bool TryGetStatement(string tagName, out string sqlStatement)
+        => _sqlStatements.TryGetValue(tagName, out sqlStatement);
+
+    /// <summary>
+    /// Adds the specified tag and SQL statement to the dictionary.
+    /// </summary>
+    /// <param name="tagName">The tag to add.</param>
+    /// <param name="sqlStatement">The SQL statement to add.</param>
+    internal void Add(string tagName, string sqlStatement)
+        => _sqlStatements.Add(tagName, sqlStatement);
+
+    /// <summary>
+    /// Returns an enumerator that iterates through the SQL statements contained in the collection.
+    /// </summary>
+    /// <returns>An enumerator that can be used to iterate through the SQL statements contained in the collection.</returns>
+    public IEnumerator<TagModel> GetEnumerator()
+    {
+        foreach(var keyValuePair in _sqlStatements)
+            yield return new TagModel { Name = keyValuePair.Key, SqlStatement = keyValuePair.Value };
+    }
+
+    /// <inheritdoc cref="GetEnumerator" />
+    IEnumerator IEnumerable.GetEnumerator()
+        => this.GetEnumerator();
+
+    /// <summary>
+    /// Converts the <see cref="IYeSqlCollection" /> instance to a <see cref="string" /> object.
+    /// </summary>
+    /// <returns>A string that represents the current object of type <see cref="IYeSqlCollection" />.</returns>
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        foreach (var tagModel in this)
+            sb.Append(tagModel.ToString());
+        return sb.ToString();
+    }
+}
---
+++ src/Models/TagModel.cs
@@ -0,0 +1,24 @@
+﻿namespace YeSql.Net;
+
+/// <summary>
+/// Represents the tag with its associated SQL statement.
+/// </summary>
+public struct TagModel
+{
+    /// <summary>
+    /// Gets the name of the tag.
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Gets the SQL statement of the tag.
+    /// </summary>
+    public string SqlStatement { get; set; }
+
+    /// <summary>
+    /// Returns a string that represents the current object of type <see cref="TagModel" />.
+    /// </summary>
+    /// <returns>A string that represents the current object of type <see cref="TagModel" />.</returns>
+    public override string ToString()
+        => $"-- name: {Name}\n{SqlStatement}\n";
+}
