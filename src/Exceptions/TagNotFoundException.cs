f774e9e4518d0ee2ad5b6d7625349b0a1bd60bd9
MrDave1999 <daveseva2010@hotmail.es>
2023-01-10 11:05:38 -0500

    Updated XML comments

--- src/Exceptions/TagNotFoundException.cs
+++ src/Exceptions/TagNotFoundException.cs
@@ -3,18 +3,18 @@
 namespace YeSql.Net;
 
 /// <summary>
-/// The exception that is thrown when the tag is not found in <see cref="IYeSqlCollection"/>
+/// The exception that is thrown when the tag is not found in <see cref="IYeSqlCollection" />.
 /// </summary>
 public class TagNotFoundException : Exception
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="TagNotFoundException"/> class with a default message.
+    /// Initializes a new instance of the <see cref="TagNotFoundException" /> class with a default message.
     /// </summary>
     public TagNotFoundException() : base(ExceptionMessages.TagNotFoundDefaultMessage) { }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="TagNotFoundException"/> class with a custom message.
+    /// Initializes a new instance of the <see cref="TagNotFoundException" /> class with the a specified error message.
     /// </summary>
-    /// <param name="message">The custom message to be displayed with the exception.</param>
+    /// <param name="message">The message that describes the error.</param>
     public TagNotFoundException(string message) : base(message) { }
 }
--- src/Exceptions/YeSqlParserException.cs
+++ src/Exceptions/YeSqlParserException.cs
@@ -3,18 +3,18 @@
 namespace YeSql.Net;
 
 /// <summary>
-/// The Exception is thrown when there is an error while parsing a SQL file using the <see cref="YeSqlParser"/>.
+/// The exception is thrown when there are one or more errors while parsing a data source (e.g., SQL file) using the <see cref="YeSqlParser" />.
 /// </summary>
 public class YeSqlParserException : Exception
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="YeSqlParserException"/> class with a default error message.
+    /// Initializes a new instance of the <see cref="YeSqlParserException" /> class with a default error message.
     /// </summary>
     public YeSqlParserException() : base(ExceptionMessages.YeSqlParserDefaultMessage) { }
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="YeSqlParserException"/> class with a custom error message.
+    /// Initializes a new instance of the <see cref="YeSqlParserException" /> class with the a specified error message.
     /// </summary>
-    /// <param name="message">The error message that explains the reason for the exception.</param>
+    /// <param name="message">The message that describes the error.</param>
     public YeSqlParserException(string message) : base(message) { }
 }
