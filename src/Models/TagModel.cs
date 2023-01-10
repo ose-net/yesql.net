b30ce7847c3b42b1a76f6a10ee72fd2527192905
MrDave1999 <daveseva2010@hotmail.es>
2023-01-10 11:36:44 -0500

    Added 'Deconstruct' method in 'TagModel' struct
    See https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/functional/deconstruct#user-defined-types

--- src/Models/TagModel.cs
+++ src/Models/TagModel.cs
@@ -16,6 +16,17 @@ public struct TagModel
     public string SqlStatement { get; set; }
 
     /// <summary>
+    /// Deconstructs the current <see cref="TagModel" />.
+    /// </summary>
+    /// <param name="name">The name of the current <see cref="TagModel" />.</param>
+    /// <param name="sqlStatement">The SQL statement of the current <see cref="TagModel" />.</param>
+    public void Deconstruct(out string name, out string sqlStatement)
+    {
+        name = Name;
+        sqlStatement = SqlStatement;
+    }
+
+    /// <summary>
     /// Returns a string that represents the current object of type <see cref="TagModel" />.
     /// </summary>
     /// <returns>A string that represents the current object of type <see cref="TagModel" />.</returns>
