45cf28e619f7d278aa85cfbf30f050c66089ad50
Johan Sánchez <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-10 11:07:09 -0500

    Added XML comments to exception classes (#20)

--- src/Exceptions/TagNotFoundException.cs
+++ src/Exceptions/TagNotFoundException.cs
@@ -2,9 +2,19 @@
 
 namespace YeSql.Net;
 
+/// <summary>
+/// The exception that is thrown when the tag is not found in <see cref="IYeSqlCollection" />.
+/// </summary>
 public class TagNotFoundException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TagNotFoundException" /> class with a default message.
+    /// </summary>
     public TagNotFoundException() : base(ExceptionMessages.TagNotFoundDefaultMessage) { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TagNotFoundException" /> class with the a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public TagNotFoundException(string message) : base(message) { }
 }
--- src/Exceptions/YeSqlParserException.cs
+++ src/Exceptions/YeSqlParserException.cs
@@ -2,9 +2,19 @@
 
 namespace YeSql.Net;
 
+/// <summary>
+/// The exception is thrown when there are one or more errors while parsing a data source (e.g., SQL file) using the <see cref="YeSqlParser" />.
+/// </summary>
 public class YeSqlParserException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="YeSqlParserException" /> class with a default error message.
+    /// </summary>
     public YeSqlParserException() : base(ExceptionMessages.YeSqlParserDefaultMessage) { }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="YeSqlParserException" /> class with the a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public YeSqlParserException(string message) : base(message) { }
 }
