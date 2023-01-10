83572ed477abf7aedfc428d2b12cf6521539b257
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-09 19:47:10 -0500

    TagNotFoundException class commented

--- src/Exceptions/TagNotFoundException.cs
+++ src/Exceptions/TagNotFoundException.cs
@@ -2,9 +2,19 @@
 
 namespace YeSql.Net;
 
+/// <summary>
+/// Exception thrown when a tag is not found in the SQL file being parsed.
+/// </summary>
 public class TagNotFoundException : Exception
 {
+    /// <summary>
+    /// Creates a new instance of the TagNotFoundException class with a default message.
+    /// </summary>
     public TagNotFoundException() : base(ExceptionMessages.TagNotFoundDefaultMessage) { }
 
+    /// <summary>
+    /// Creates a new instance of the TagNotFoundException class with a custom message.
+    /// </summary>
+    /// <param name="message">The custom message to be displayed with the exception.</param>
     public TagNotFoundException(string message) : base(message) { }
 }
