0024325d4691997765367100319430629db92392
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-09 19:49:16 -0500

    YeSqlParserException class commented

--- src/Exceptions/YeSqlParserException.cs
+++ src/Exceptions/YeSqlParserException.cs
@@ -2,9 +2,19 @@
 
 namespace YeSql.Net;
 
+/// <summary>
+/// The YeSqlParserException is thrown when there is an error while parsing a SQL file using the YeSqlParser.
+/// </summary>
 public class YeSqlParserException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the YeSqlParserException class with a default error message.
+    /// </summary>
     public YeSqlParserException() : base(ExceptionMessages.YeSqlParserDefaultMessage) { }
 
+    /// <summary>
+    /// Initializes a new instance of the YeSqlParserException class with a custom error message.
+    /// </summary>
+    /// <param name="message">The error message that explains the reason for the exception.</param>
     public YeSqlParserException(string message) : base(message) { }
 }
