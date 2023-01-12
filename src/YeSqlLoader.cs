b00313b8cd134f976c408118fbf685815f0aeb88
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-12 16:32:16 -0500

    Created YeSqlLoader class

---
+++ src/YeSqlLoader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YeSql.Net;
+
+public class YeSqlLoader
+{
+    private YeSqlParser _parser;
+
+    private YeSqlValidationResult _validationResult;
+
+    public YeSqlLoader()
+    {
+        _parser = new YeSqlParser();
+        _validationResult = new YeSqlValidationResult();
+    }
+
+    public IYeSqlCollection Load()
+    {
+        throw new NotImplementedException();
+    }
+
+    public IYeSqlCollection Load(params string[] files)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IYeSqlCollection Load(string directoryName)
+    {
+        throw new NotImplementedException();
+    }
+}
