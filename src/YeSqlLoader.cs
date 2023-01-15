674cdae5770aa06f994502a9df5fb7875f0958c8
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-14 19:40:21 -0500

    implemented methods in YeSqlLoader

--- src/YeSqlLoader.cs
+++ src/YeSqlLoader.cs
@@ -1,20 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
 
 namespace YeSql.Net;
 
 public class YeSqlLoader
 {
-    private YeSqlParser _parser;
+    private readonly YeSqlParser _parser = new();
 
-    private YeSqlValidationResult _validationResult;
-
-    public YeSqlLoader()
-    {
-        _parser = new YeSqlParser();
-        _validationResult = new YeSqlValidationResult();
-    }
+    private YeSqlValidationResult _validationResult = new();
 
     public IYeSqlCollection Load()
     {
@@ -23,11 +18,109 @@ public class YeSqlLoader
 
     public IYeSqlCollection Load(params string[] files)
     {
-        throw new NotImplementedException();
+        var filesSql = GetSqlFileContents(files);
+
+        if (_validationResult.HasError())
+            throw new AggregateException();
+
+        foreach (var file in filesSql)
+        {
+            _parser.SqlFileName = file.FileName;
+            _parser.Parse(file.Content, out _);
+        }
+
+        return _parser.SqlStatements;
     }
 
     public IYeSqlCollection Load(string directoryName)
     {
-        throw new NotImplementedException();
+        if (directoryName is null)
+            _validationResult.Add($"{nameof(directoryName)} is null");
+
+        var files = GetSqlFileContents(directoryName);
+
+        if (!files.Any())
+            _validationResult.Add(string.Format(ExceptionMessages.NoneFileFoundInSpecifiedDirectoryMessage, directoryName));
+
+        if (_validationResult.HasError())
+            throw new AggregateException();
+        
+        foreach (var file in files)
+        {
+            _parser.SqlFileName = file.FileName;
+            _parser.Parse(file.Content, out _);         
+        }
+
+        return _parser.SqlStatements;
     }
+
+
+    private bool IsSqlFile(string fileName)
+    => new FileInfo(fileName).Extension == "sql";
+
+    private IEnumerable<SqlFile> GetSqlFileContents(string[] files)
+    {
+        if (files is null)
+            _validationResult.Add($"{nameof(files)} is null");
+
+        foreach (var file in files)
+        {
+            var fileInfo = new FileInfo(file);
+            var content = string.Empty;
+
+            try
+            {
+                content = File.ReadAllText(file);
+            }
+            catch (FileNotFoundException)
+            {
+                _validationResult.Add(string.Format(ExceptionMessages.FileNotFoundMessage, file));
+            }
+
+            if (string.IsNullOrWhiteSpace(content))
+                _validationResult.Add(ExceptionMessages.DataSourceIsEmptyOrWhitespaceMessage);
+
+            if (fileInfo.Extension != "sql")
+                _validationResult.Add("The file is not sql.");
+
+            if (file is null)
+                _validationResult.Add($"{nameof(files)} is null");
+
+            yield return new SqlFile
+            {
+                FileName = fileInfo.Name,
+                Content = content
+            };
+        }
+
+    }
+
+    private IEnumerable<SqlFile> GetSqlFileContents(string directoryName)
+    {
+        var files = Directory.GetFiles(directoryName, "*.sql", SearchOption.AllDirectories);
+
+        foreach(var file in files)
+        {
+            var content = File.ReadAllText(file);
+            var name = new FileInfo(file).Name;
+
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                _validationResult.Add($"{name} {ExceptionMessages.DataSourceIsEmptyOrWhitespaceMessage}");
+            }
+
+            yield return new SqlFile
+            {
+                Content = content,
+                FileName = name
+            };
+        }
+    }
+
+    private class SqlFile
+    {
+        public string FileName { get; set; }
+        public string Content { get; set; }
+    }
+
 }
