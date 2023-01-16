7eb7b3e0d4d6c4d1006d2f179d3a2aa4ea8def8d
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-16 14:55:33 -0500

    Added new default message for YeSqlLoaderException

--- src/Models/ExceptionMessages.cs
+++ src/Models/ExceptionMessages.cs
@@ -8,6 +8,7 @@ public class ExceptionMessages
     public const string FileNotFoundMessage                       = "{0}: error: No such file or directory.";
     public const string NoneFileFoundInSpecifiedDirectoryMessage  = "error: No sql file found in the directory '{0}'.";
     public const string YeSqlParserDefaultMessage                 = "error: Parser found syntax errors.";
+    public const string YeSqlLoaderDefaultMessage                 = "error: Loader found an error while loading the SQL file.";
     public const string DataSourceIsEmptyOrWhitespaceMessage      = "Data source is empty or consists only in whitespace.";
     public const string TagNotFoundMessage                        = "The given tag '{0}' was not present in the collection.";
     public const string TagNotFoundDefaultMessage                 = "error: No tag found in the collection.";
