8dea9bb438bd7135c50d02ace49a0bdcdf216067
MrDave1999 <daveseva2010@hotmail.es>
2023-01-17 18:36:59 -0500

    Added 'Parse' method with parameter specifying SQL file name
    * SqlFileName property has been converted to a private field
    
    * Updated XML comments

--- src/Parser/YeSqlParser.cs
+++ src/Parser/YeSqlParser.cs
@@ -8,6 +8,11 @@ namespace YeSql.Net;
 public partial class YeSqlParser
 {
     /// <summary>
+    /// The name of the SQL file that caused an error.
+    /// </summary>
+    private string _sqlFileName;
+
+    /// <summary>
     /// Gets the dictionary containing the SQL statements that have been parsed from the data source (e.g., a SQL file).
     /// </summary>
     internal YeSqlDictionary SqlStatements { get; } = new();
@@ -18,20 +23,19 @@ public partial class YeSqlParser
     internal YeSqlValidationResult ValidationResult { get; } = new();
 
     /// <summary>
-    /// Gets the name of the file from which the SQL statements were parsed.
-    /// </summary>
-    internal string SqlFileName { get; }
-
-    /// <summary>
     /// Start the parsing to extract the SQL statements from a data source.
     /// </summary>
     /// <param name="source">The data source to parsing.</param>
+    /// <param name="sqlFileName">The name of the SQL file that caused an error.</param>
     /// <returns>A collection containing the tags with their associated SQL statements.</returns>
     /// <exception cref="ArgumentNullException"><c>source</c> is <c>null</c>.</exception>
-    internal IYeSqlCollection Parse(string source)
-        => Parse(source, out _);
+    internal IYeSqlCollection Parse(string source, string sqlFileName)
+    {
+        _sqlFileName = sqlFileName;
+        return Parse(source, out _);
+    }
 
-    /// <inheritdoc cref="Parse(string)" />
+    /// <inheritdoc cref="Parse(string, string)" />
     /// <param name="validationResult">The validation result of the parsing process.</param>
     public IYeSqlCollection Parse(string source, out YeSqlValidationResult validationResult)
     {
