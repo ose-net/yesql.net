9d0238933509057ed769f32225ae0eb3fea5d846
Ts-Pytham <43942761+Ts-Pytham@users.noreply.github.com>
2023-01-16 21:12:17 -0500

    Created YeSqlLoader.HelperMethods class and documentation

---
+++ src/Loader/YeSqlLoader.HelperMethods.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace YeSql.Net;
+
+public partial class YeSqlLoader
+{
+
+    /// <summary>
+    /// Nested class that is used to store the file name and contents of an SQL file.
+    /// </summary>
+    private class SqlFile
+    {
+        /// <summary>
+        /// Gets or sets the file name of the SQL file.
+        /// </summary>
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the contents of the SQL file.
+        /// </summary>
+        public string Content { get; set; }
+    }
+
+    /// <summary>
+    /// Checks for any validation errors that may have occurred during the loading process, and throws an exception if there are any.
+    /// If there is validation errors found in <see cref="YeSqlLoader"/>, it throws an <see cref="YeSqlLoaderException"/>,
+    /// If there is validation errors found in <see cref="YeSqlParser"/>, it throws an <see cref="YeSqlParserException"/>,
+    /// If there is validation errors found in both <see cref="YeSqlLoader"/> and <see cref="YeSqlParser"/>, it throws an <see cref="AggregateException"/> that contains both exception.
+    /// </summary>
+    /// <exception cref="YeSqlLoaderException">Thrown when there are validation errors in <see cref="YeSqlLoader"/>.</exception>
+    /// <exception cref="YeSqlParserException">Thrown when there are validation errors in <see cref="YeSqlParser"/>.</exception>
+    /// <exception cref="AggregateException">Thrown when there are validation errors in <see cref="YeSqlLoader"/> and <see cref="YeSqlParser"/>.</exception>
+    private void CreateAndThrowExceptions()
+    {
+        var exceptions = new List<Exception>();
+
+        if (_validationResult.HasError())
+            exceptions.Add(new YeSqlLoaderException(_validationResult.ErrorMessages));
+
+        if (_parser.ValidationResult.HasError())
+            exceptions.Add(new YeSqlParserException(_parser.ValidationResult.ErrorMessages));
+
+        if (exceptions.Count == 0)
+            throw new AggregateException(exceptions);
+    }
+
+
+    /// <summary>
+    /// Retrieves the contents of the specified SQL files and returns them as an enumerable collection of <see cref="SqlFile"/> objects.
+    /// </summary>
+    /// <param name="files">An array of file paths of the SQL files to be loaded.</param>
+    /// <returns>An enumerable collection of <see cref="SqlFile"/> objects that contains the file name and contents of the SQL files.</returns>
+    private IEnumerable<SqlFile> GetSqlFileContents(string[] files)
+    {
+        foreach (var file in files)
+        {
+            if (file is null)
+            {
+                _validationResult.Add($"{nameof(files)} is null");
+                continue;
+            }
+
+            var fileInfo = new FileInfo(file);
+            var content = string.Empty;
+
+            try
+            {
+                content = File.ReadAllText(file);
+            }
+            catch (FileNotFoundException)
+            {
+                _validationResult.Add(string.Format(ExceptionMessages.FileNotFoundMessage, file));
+            }
+
+            if (string.IsNullOrWhiteSpace(content))
+                _validationResult.Add(ExceptionMessages.DataSourceIsEmptyOrWhitespaceMessage);
+
+            if (fileInfo.Extension != "sql")
+                _validationResult.Add("The file is not sql.");
+
+            yield return new SqlFile
+            {
+                FileName = fileInfo.Name,
+                Content = content
+            };
+        }
+
+    }
+
+    /// <summary>
+    /// Retrieves the contents of the SQL files located in the specified directory and its subdirectories, and returns them as an enumerable collection of <see cref="SqlFile"/> objects.
+    /// </summary>
+    /// <param name="directoryName">The path of the directory where the SQL files are located.</param>
+    /// <returns>An enumerable collection of <see cref="SqlFile"/> objects that contains the file name and contents of the SQL files.</returns>
+    private IEnumerable<SqlFile> GetSqlFileContents(string directoryName)
+    {
+        var files = Directory.GetFiles(directoryName, "*.sql", SearchOption.AllDirectories);
+
+        foreach (var file in files)
+        {
+            var content = File.ReadAllText(file);
+            var name = new FileInfo(file).Name;
+
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                _validationResult.Add($"{name} {ExceptionMessages.DataSourceIsEmptyOrWhitespaceMessage}");
+            }
+
+            yield return new SqlFile
+            {
+                Content = content,
+                FileName = name
+            };
+        }
+    }
+}
---
+++ src/Loader/YeSqlLoader.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace YeSql.Net;
+
+/// <summary>
+/// The <see cref="YeSqlLoader"/> class is a partial class that provides the functionality for loading SQL files into the system.
+/// </summary>
+public partial class YeSqlLoader
+{
+    /// <summary>
+    ///  An instance of <see cref="YeSqlParser"/> class, used to parse SQL files before loading into the system.
+    /// </summary>
+    private readonly YeSqlParser _parser = new();
+
+    /// <summary>
+    ///  An instance of <see cref="YeSqlValidationResult"/> class, used to store the result of validating SQL files before loading them into the system.
+    /// </summary>
+    private readonly YeSqlValidationResult _validationResult = new();
+
+    /// <summary>
+    /// Loads all SQL files found in the root of the project.
+    /// </summary>
+    /// <returns>An instance of <see cref="IYeSqlCollection"/> interface that contains the loaded SQL files</returns>
+    public IYeSqlCollection Load()
+    {
+        return Load(AppContext.BaseDirectory);
+    }
+
+    /// <summary>
+    /// Loads the SQL files from the specified file paths.
+    /// </summary>
+    /// <param name="files">An array of file paths of the SQL files to be loaded.</param>
+    /// <returns>An instance of <see cref="IYeSqlCollection"/> interface that contains the loaded SQL files.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the files parameter is null.</exception>
+    public IYeSqlCollection Load(params string[] files)
+    {
+        if (files is null)
+            throw new ArgumentNullException(nameof(files));
+
+        var filesSql = GetSqlFileContents(files);
+
+        foreach (var file in filesSql)
+        {
+            _parser.SqlFileName = file.FileName;
+            _parser.Parse(file.Content, out _);
+        }
+
+        CreateAndThrowExceptions();
+
+        return _parser.SqlStatements;
+    }
+
+    /// <summary>
+    /// Loads the SQL files from the specified directory.
+    /// </summary>
+    /// <param name="directoryName">The path of the directory where the SQL files are located.</param>
+    /// <returns>An instance of <see cref="IYeSqlCollection"/> interface that contains the loaded SQL files.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when the directoryName parameter is null.</exception>
+    /// <exception cref="AggregateException">Thrown when there are validation errors on the files or if no files were found in the specified directory.</exception>
+    public IYeSqlCollection Load(string directoryName)
+    {
+        if (directoryName is null)
+            throw new ArgumentNullException(nameof(directoryName));
+
+        var files = GetSqlFileContents(directoryName);
+
+        if (!files.Any())
+            _validationResult.Add(string.Format(ExceptionMessages.NoneFileFoundInSpecifiedDirectoryMessage, directoryName));
+
+        foreach (var file in files)
+        {
+            _parser.SqlFileName = file.FileName;
+            _parser.Parse(file.Content, out _);
+        }
+
+        CreateAndThrowExceptions();
+
+        return _parser.SqlStatements;
+    }
+}
