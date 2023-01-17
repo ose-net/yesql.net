4e299029ed874bb1b6489e8aa60d86272845e209
MrDave1999 <daveseva2010@hotmail.es>
2023-01-17 18:06:20 -0500

    Added XML comments for IsEmpty

--- src/Extensions/EnumerableExtensions.cs
+++ src/Extensions/EnumerableExtensions.cs
@@ -5,6 +5,12 @@ namespace YeSql.Net;
 
 internal static class EnumerableExtensions
 {
-    public static bool IsEmpty<T>(this IEnumerable<T> values)
-        => !values.Any();
+    /// <summary>
+    /// Determines if a sequence is empty.
+    /// </summary>
+    /// <typeparam name="T">The type of the elements of <c>source</c>.</typeparam>
+    /// <param name="source">The <see cref="IEnumerable{T}"/> to check.</param>
+    /// <returns><c>true</c> if the sequence is empty, otherwise <c>false</c>.</returns>
+    public static bool IsEmpty<T>(this IEnumerable<T> source)
+        => !source.Any();
 }
