1dce0e04e7140348df2b2c939aaed663a9609abc
MrDave1999 <daveseva2010@hotmail.es>
2023-05-23 13:17:25 -0500

    Updated global usings

--- tests/Usings.cs
+++ tests/Usings.cs
@@ -1,6 +1,6 @@
-global using static System.Environment;
 global using NUnit.Framework;
 global using FluentAssertions;
+global using FluentAssertions.Collections;
 global using System.Collections;
 global using YeSql.Net;
 global using YeSql.Net.Tests.Parser.TestCases;
