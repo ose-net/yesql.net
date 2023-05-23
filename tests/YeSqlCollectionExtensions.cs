9e365c96f20855f7f5fc3e1867ccd483505fa8be
MrDave1999 <daveseva2010@hotmail.es>
2023-05-23 13:17:47 -0500

    Removed YeSqlCollectionExtensions file

--- tests/YeSqlCollectionExtensions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace YeSql.Net.Tests;
-
-public static class YeSqlCollectionExtensions
-{
-    public static Dictionary<string, string> ToDictionary(this IYeSqlCollection collection)
-        => collection.ToDictionary(model => model.Name, model => model.SqlStatement);
-}
