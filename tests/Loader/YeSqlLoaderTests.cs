7ad77ce6d8462ae84ef8f9b721060484f1ae07a1
Johan Sánchez <43942761+Ts-Pytham@users.noreply.github.com>
2023-06-09 11:24:50 -0500

    tests: Added integration tests for the loader (#54)

---
+++ tests/Loader/YeSqlLoaderTests.cs
@@ -0,0 +1,143 @@
+﻿namespace YeSql.Net.Tests.Loader;
+
+public class YeSqlLoaderTests
+{
+    [Test]
+    public void LoadFromFiles_WhenSqlFilesIsNull_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        var loader = new YeSqlLoader();
+
+        // Act
+        Action action = () => loader.LoadFromFiles(null);
+
+        // Assert
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void LoadFromFiles_WhenSqlFilesExists_ShouldReturnsYeSqlCollection()
+    {
+        // Arrange
+        var loader = new YeSqlLoader();
+        var path = CreateSqlFile();
+
+        // Act
+        var collection = loader.LoadFromFiles(path);
+        File.Delete(path);
+
+        // Assert
+        collection.Should().NotBeNull();
+    }
+
+    [Test]
+    public void LoadFromFiles_WhenFileNotHaveExtension_ShouldThrowAggregateException()
+    {
+        // Arrange
+        var loader = new YeSqlLoader();
+        var path = CreateFile();
+        var expectedMessage = string.Format(ExceptionMessages.FileHasNotSqlExtension, path);
+
+        // Act
+        Action action = () => loader.LoadFromFiles(path);
+
+        // Asserts
+        action.Should().Throw<YeSqlLoaderException>()
+                       .WithMessage(expectedMessage);
+        action.Should().Throw<AggregateException>();
+    }
+
+    [Test]
+    public void LoadFromFiles_WhenSqlFileNotExists_ShouldThrowAggregateException()
+    {
+        // Arrange
+        var loader = new YeSqlLoader();
+        var path = $"{Directory.GetCurrentDirectory()}/test.sql";
+        var expectedMessage = string.Format(ExceptionMessages.FileNotFound, path);
+
+        // Act
+        Action action = () => loader.LoadFromFiles(path);
+
+        // Asserts
+        action.Should().Throw<YeSqlLoaderException>()
+                       .WithMessage(expectedMessage);
+        action.Should().Throw<AggregateException>();
+    }
+
+
+    [Test]
+    public void LoadFromDirectories_WhenDirectoriesPathIsNull_ShouldThrowArgumentNullException()
+    {
+        // Arrange
+        var loader = new YeSqlLoader();
+
+        // Act
+        Action action = () => loader.LoadFromDirectories(null);
+
+        // Assert
+        action.Should().Throw<ArgumentNullException>();
+    }
+
+    [Test]
+    public void LoadFromDirectories_WhenNotExistsSqlFilesInDirectory_ShouldThrowAggregateException()
+    {
+        // Arrange
+        var loader = new YeSqlLoader();
+        var path = Directory.GetCurrentDirectory();
+        var expectedMessage = string.Format(ExceptionMessages.NoneFileFoundInSpecifiedDirectory, path);
+
+        // Act
+        Action action = () => loader.LoadFromDirectories(path);
+
+        // Asserts
+        action.Should().Throw<YeSqlLoaderException>()
+                       .WithMessage(expectedMessage);
+        action.Should().Throw<AggregateException>();
+    }
+
+    [Test]
+    public void LoadFromDirectories_WhenSqlFilesExistsInDirectory_ShouldReturnsYeSqlCollection()
+    {
+        // Arrange
+        var loader = new YeSqlLoader();
+        var pathDirectory = Directory.GetCurrentDirectory();
+        var pathFile = CreateSqlFile();
+
+        // Act
+        var collection = loader.LoadFromDirectories(pathDirectory);
+        File.Delete(pathFile);
+
+        // Assert
+        collection.Should().NotBeNull();
+    }
+
+    private static string CreateSqlFile()
+    {
+        var path = $"{Directory.GetCurrentDirectory()}/test.sql";
+        using var fileStream = new FileStream(path, FileMode.Create);
+        var content = """
+                      -- name: GetUsers
+                      -- Gets user records.
+                      SELECT* FROM [user];
+                      """;
+        byte[] bytes = Encoding.UTF8.GetBytes(content);
+        fileStream.Write(bytes, 0, bytes.Length);
+        fileStream.Close();
+        return path;
+    }
+
+    private static string CreateFile()
+    {
+        var path = $"{Directory.GetCurrentDirectory()}/test.txt";
+        using var fileStream = new FileStream(path, FileMode.Create);
+        var content = """
+                      -- name: GetUsers
+                      -- Gets user records.
+                      SELECT* FROM [user];
+                      """;
+        byte[] bytes = Encoding.UTF8.GetBytes(content);
+        fileStream.Write(bytes, 0, bytes.Length);
+        fileStream.Close();
+        return path;
+    }
+}
--- tests/Usings.cs
+++ tests/Usings.cs
@@ -2,5 +2,7 @@ global using NUnit.Framework;
 global using FluentAssertions;
 global using FluentAssertions.Collections;
 global using System.Collections;
+global using System.IO;
+global using System.Text;
 global using YeSql.Net;
 global using YeSql.Net.Tests.Parser.TestCases;
