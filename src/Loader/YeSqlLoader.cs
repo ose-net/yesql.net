19d24fab01db334e0720fd2099213190088929a7
Ts-Pytham <johansanchezdeleon@gmail.com>
2023-06-13 00:40:21 -0500

    documentation created

--- src/Loader/YeSqlLoader.cs
+++ src/Loader/YeSqlLoader.cs
@@ -22,6 +22,7 @@ public partial class YeSqlLoader
     /// </summary>
     /// <param name="sqlFiles">The SQL files to load.</param>
     /// <returns>A collection containing the tags with their associated SQL statements.</returns>
+    /// <exception cref="ArgumentException">One or more files in <paramref name="sqlFiles"/> is <c>null</c>, empty or whitespace.</exception>
     /// <exception cref="ArgumentNullException"><c>sqlFiles</c> is <c>null</c>.</exception>
     /// <exception cref="AggregateException">If the parser and/or loader encounters one or more errors.</exception>
     public IYeSqlCollection LoadFromFiles(params string[] sqlFiles)
@@ -46,6 +47,7 @@ public partial class YeSqlLoader
     /// </summary>
     /// <param name="directories">A set of directories where the SQL files are located.</param>
     /// <returns>A collection containing the tags with their associated SQL statements.</returns>
+    /// <exception cref="ArgumentException">One or more directories in <paramref name="directories"/> is <c>null</c>, empty or whitespace.</exception>
     /// <exception cref="ArgumentNullException"><c>directoryName</c> is <c>null</c>.</exception>
     /// <exception cref="AggregateException">If the parser and/or loader encounters one or more errors.</exception>
     public IYeSqlCollection LoadFromDirectories(params string[] directories)
