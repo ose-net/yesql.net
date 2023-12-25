c0e07715c9c71459d4cfd2b3b5d20eb881a76f95
MrDave1999 <daveseva2010@hotmail.es>
2023-12-25 18:00:45 -0500

    chore: Add a directory called Resources

---
+++ tests/Loader/Resources/error/errors.sql
@@ -0,0 +1,16 @@
+-- file: errors_1.sql
+SELECT * FROM users;
+-- name: GetProducts
+SELECT id, name, price FROM products;
+
+-- name: GetUsers
+SELECT id, name, email FROM users;
+
+-- name:
+SELECT name FROM roles;
+
+-- name:  GetUsers    
+SELECT name FROM users;
+
+-- name:          
+SELECT * FROM roles;
